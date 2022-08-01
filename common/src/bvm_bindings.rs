2a13d88ca27a2a2ad2099138758e46b81f006567
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2022-08-01 17:29:22 +0300

    Add bindings for reading variables from app shader

--- common/src/bvm_bindings.rs
+++ common/src/bvm_bindings.rs
@@ -5,6 +5,7 @@ pub mod root {
     pub type AssetID = u32;
     pub type Timestamp = u64;
     pub type ContractID = [u8; 32usize];
+    pub type ShaderID = [u8; 32usize];
     pub type HashValue = [u8; 32usize];
     pub type Secp_scalar_data = [u8; 32usize];
 
@@ -46,6 +47,57 @@ pub mod root {
     pub mod Env {
         #[allow(unused_imports)]
         use self::super::super::root;
+        use crate::root::ContractID;
+
+        #[repr(packed(1))]
+        pub struct KeyPrefix {
+            pub cid: ContractID,
+            pub tag: u8,
+        }
+
+        #[repr(packed(1))]
+        pub struct Key_T<T> {
+            pub prefix: KeyPrefix,
+            pub key_in_contract: T,
+        }
+
+        pub struct VarReaderEx<const flexible: bool> {
+            handle: u32,
+        }
+
+        impl<const flexible: bool> VarReaderEx<flexible> {
+            fn EnumInternal(&mut self, key1: *const u32, key1_size: u32, key2: *const u32, key2_size: u32) {
+                self.handle = Vars_Enum(key1, key1_size, key2, key2_size); 
+            }
+
+            fn CloseInternal(&self) {
+                if flexible {
+                    if self.handle != 0 {
+                        return;
+                    }
+                }
+                Vars_Close(self.handle);
+            }
+
+            pub fn MoveNext(&self, key: *mut u32, key_size: &mut u32, val: *mut u32, val_size: &mut u32, repeat: u8) -> bool {
+                Vars_MoveNext(self.handle, key, key_size, val, val_size, 0) != 0
+            }
+
+            pub fn Read<K, V>(key: *const K, key_size: u32, value: *mut V, val_size: u32) -> bool {
+                let mut r = VarReaderEx::<false> {
+                    handle: Default::default(),
+                };
+                r.EnumInternal(key as *const u32, key_size, key as *const u32, key_size);
+
+                let mut key_size: u32 = 0;
+                let mut val_size: u32 = val_size;
+                let ret = r.MoveNext(0 as *mut u32, &mut key_size, value as *mut u32, &mut val_size, 0);
+                r.CloseInternal();
+                ret
+            }
+        }
+
+        pub type VarReader = VarReaderEx<false>;
 
         pub fn SaveVar<K, V>(
                 key: *const K,
